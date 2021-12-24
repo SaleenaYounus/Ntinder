653da08f48268dd0032ae28336b5f8133e3855de
SaleenaYounus <saleenans1@gmail.com>
2021-12-24 14:05:52 +0530

    V7

--- app/src/main/java/com/example/ntinder/MainActivity.java
+++ app/src/main/java/com/example/ntinder/MainActivity.java
@@ -1,16 +1,14 @@
 package com.example.ntinder;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -25,31 +23,47 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-    private cards  card_data[];
+    private cards cards_data[];
+
     private arrayAdapter arrayAdapter;
     private int i;
 
     private FirebaseAuth mAuth;
     private String currentUid;
+    private DatabaseReference usersDb;
 
-    ListView  listView;
+
+    ListView listView;
     List<cards> rowItems;
 
+    public MainActivity() {
+    }
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        usersDb = FirebaseDatabase.getInstance().getReference().child("Users");
+
         mAuth = FirebaseAuth.getInstance();
+        currentUid = mAuth.getCurrentUser().getUid();
+
         checkUserSex();
+
+
         rowItems = new ArrayList<cards>();
-        arrayAdapter = new arrayAdapter(this, R.layout.item,rowItems);
-      /*  al.add("javascript"); if you add this line to the program , we can only see the oppositesex username after swiping  the javascript card.
-        arrayAdapter.notifyDataSetChanged();*/
 
-        SwipeFlingAdapterView flingContainer = (SwipeFlingAdapterView) findViewById(R.id.frame);
+        arrayAdapter = new arrayAdapter(this,  R.layout.item, rowItems);
 
 
+
+        SwipeFlingAdapterView flingContainer = findViewById(R.id.frame);
         flingContainer.setAdapter(arrayAdapter);
         flingContainer.setFlingListener(new SwipeFlingAdapterView.onFlingListener() {
             @Override
@@ -65,18 +79,23 @@ public class MainActivity extends AppCompatActivity {
                 //Do something on the left!
                 //You also have access to the original object.
                 //If you want to use it just cast it (String) dataObject
-                Toast.makeText(MainActivity.this, "left", Toast.LENGTH_SHORT).show();
-
-
+                cards obj = (cards) dataObject;
+                String userId = obj.getUserId();
+                usersDb.child(oppositeUserSex).child(userId).child("connections").child("nope").child(currentUid).setValue(true);
+                Toast.makeText(MainActivity.this, "Left!", Toast.LENGTH_SHORT).show();
             }
 
             @Override
             public void onRightCardExit(Object dataObject) {
-                Toast.makeText(MainActivity.this, "right", Toast.LENGTH_SHORT).show();
+                cards obj = (cards) dataObject;
+                String userId = obj.getUserId();
+                usersDb.child(oppositeUserSex).child(userId).child("connections").child("yeps").child(currentUid).setValue(true);
+                Toast.makeText(MainActivity.this, "Right!", Toast.LENGTH_SHORT).show();
             }
 
             @Override
             public void onAdapterAboutToEmpty(int itemsInAdapter) {
+
             }
 
             @Override
@@ -87,49 +106,49 @@ public class MainActivity extends AppCompatActivity {
 
 
         // Optionally add an OnItemClickListener
-        flingContainer.setOnItemClickListener(new SwipeFlingAdapterView.OnItemClickListener() {
+        flingContainer.setOnItemClickListener((itemPosition, dataObject) -> Toast.makeText(MainActivity.this, "Item Clicked!", Toast.LENGTH_SHORT).show());
+
+    }
+    public String userSex;
+    private String oppositeUserSex;
+    public  void checkUserSex() {
+        final FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+        DatabaseReference maleDb = FirebaseDatabase.getInstance().getReference().child("Users").child("Male");
+        maleDb.addChildEventListener(new ChildEventListener() {
             @Override
-            public void onItemClicked(int itemPosition, Object dataObject) {
-                Toast.makeText(MainActivity.this, "Click", Toast.LENGTH_LONG).show();
+            public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+                if (snapshot.getKey().equals(user.getUid())) {
+                    userSex= "Male";
+                    oppositeUserSex = "Female";
+                    getOppositeSexUsers();
+                }
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot snapshot) {
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
             }
         });
 
-    }
-    private  String userSex;
-    private  String oppositeUserSex;
-    public  void checkUserSex(){
-        final FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-
-    DatabaseReference maleDb = FirebaseDatabase.getInstance().getReference().child("Users").child("Male");
-    maleDb.addChildEventListener(new ChildEventListener() {
-        @Override
-        public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
-            if(snapshot.getKey().equals(user.getUid())){
-                userSex = "Male";
-                oppositeUserSex ="Female";
-                getOppositeSexUsers();
-            }
-        }
-        @Override
-        public void onChildChanged(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
-        }
-        @Override
-        public void onChildRemoved(@NonNull DataSnapshot snapshot) {
-        }
-        @Override
-        public void onChildMoved(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
-        }
-        @Override
-        public void onCancelled(@NonNull DatabaseError error) {
-        }
-    });
         DatabaseReference femaleDb = FirebaseDatabase.getInstance().getReference().child("Users").child("Female");
         femaleDb.addChildEventListener(new ChildEventListener() {
             @Override
             public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
                 if(snapshot.getKey().equals(user.getUid())){
                     userSex = "Female";
-                    oppositeUserSex ="Male";
+                    oppositeUserSex = "Male";
                     getOppositeSexUsers();
                 }
             }
@@ -146,45 +165,45 @@ public class MainActivity extends AppCompatActivity {
             public void onCancelled(@NonNull DatabaseError error) {
             }
         });
-  }
+    }
 
-  public void getOppositeSexUsers(){
-      DatabaseReference oppositeSexDb= FirebaseDatabase.getInstance().getReference().child("Users").child(oppositeUserSex);
-      oppositeSexDb.addChildEventListener(new ChildEventListener() {
-          @Override
-          public void onChildAdded( DataSnapshot snapshot,  String s) {
-              if(snapshot.exists()){
-                  cards Item =new  cards(snapshot.getKey(), snapshot.child("name").getValue().toString());
-                  rowItems.add(Item);
-                  arrayAdapter.notifyDataSetChanged();
+    public void getOppositeSexUsers() {
+        DatabaseReference oppositeSexDb = FirebaseDatabase.getInstance().getReference().child("Users").child(oppositeUserSex);
+        oppositeSexDb.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+                if (snapshot.exists() && !snapshot.child("connections").child("nope").hasChild(currentUid) && !snapshot.child("connections").child("yeps").hasChild(currentUid)) {
+                    cards Item = new cards(snapshot.getKey(), snapshot.child("name").getValue().toString());
+                    rowItems.add(Item);
+                    arrayAdapter.notifyDataSetChanged();
+                }
+            }
 
-              }
-          }
-          @Override
-          public void onChildChanged(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
-          }
-          @Override
-          public void onChildRemoved(@NonNull DataSnapshot snapshot) {
-          }
-          @Override
-          public void onChildMoved(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
-          }
-          @Override
-          public void onCancelled(@NonNull DatabaseError error) {
-          }
-      });
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+            }
 
-  }
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot snapshot) {
+            }
 
-    public void logoutUser(View view) {
-        mAuth.signOut();
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+            }
+        });
+
+    }
+
+    public void LogoutUser(View view) {
+        mAuth.signOut();;
         Intent intent = new Intent(MainActivity.this, ChooseLoginRegistrationActivity.class);
         startActivity(intent);
         finish();
         return;
+
     }
-
 }
-
-
-
